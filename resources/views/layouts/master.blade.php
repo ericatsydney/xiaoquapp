84a7241808dd89b350e8f9642efbac5367f2a289
Eric Tan <eric.g.x.tan@gmail.com>
2015-12-25 07:40:13 +1000

    Update the template and bootstrap

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+@include('layouts.page-head')
+<body>
+<div class="container">
+  @yield('content')
+</div>
+</body>
+</html>
---
+++ resources/views/layouts/page-head.blade.php
@@ -0,0 +1,10 @@
+<head>
+  <title>Laravel</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+  <script src="//https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  <link href="{{ URL::asset('/css/freelancer.css') }}" rel="stylesheet">
+</head>
